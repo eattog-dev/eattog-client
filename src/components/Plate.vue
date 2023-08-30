565209b2474970eae5b2178f8e01e8e34e3319af
karinaoxiru <ra187115@ucdb.br>
2023-08-29 20:04:37 -0400

    comitei

--- src/components/Plate.vue
+++ src/components/Plate.vue
@@ -2,14 +2,16 @@
   <el-row>
     <el-col
       :span="8"
-      v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 2 : 0" >
-      
+      v-for="(o, index) in 1"
+      :key="o"
+      :offset="index > 0 ? 2 : 0"
+    >
       <el-card :body-style="{ padding: '0px' }">
         <img src="../assets/img-pratos/pacu-recheado.jpg" class="image" />
         <div style="padding: 14px">
           <p
-            class="dish-content__container dish-content__details"
-            data-test-id="dish-content__details"
+            class="dish-content_container dish-contentdetails"
+            data-test-id="dish-content_details"
           >
             Marmitex: Pacu recheado, com farofa
           </p>
@@ -21,8 +23,7 @@
               class="dish-price__discount"
               data-test-id="dish-content-discount-price"
             >
-              R$ 45,00
-              <span class="dish-price__original">R$ 57,00</span>
+              R$ 45,00 <span class="dish-price__original">R$ 57,00</span>
             </span>
           </div>
           <div class="dish-restaurant" href="">
@@ -45,8 +46,55 @@
     </el-col>
   </el-row>
 </template>
-
+<!--
+  <script>
+  export default {
+    data() {
+      return {
+          Image: [
+              "../assets/img-pratos/pacu-recheado.jpg",
+              "../assets/img-pratos/lanche.jpg",
+              "../assets/img-pratos/feijao-tropeiro.jpg",
+              "../assets/img-pratos/moqueca-pantaneira.jpg",
+              "../assets/img-pratos/pintado-na-telha.jpg",
+              "../assets/img-pratos/soba-pantaneiro.jpg",
+              "../assets/img-pratos/salada-pantaneira.jpg"
+          ]
+      }
+    }
+  }
+  </script>
+  -->
 <style>
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
 .dish-price__discount {
   color: #50a773;
   margin-bottom: 10px;
