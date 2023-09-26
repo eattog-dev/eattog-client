298a90c1123d153eb889dcfeb575da646d055a36
MilenaSCarmona <30530241+MilenaSCarmona@users.noreply.github.com>
2023-09-26 18:53:30 -0400

    add new name class

--- src/components/OrderStates.vue
+++ src/components/OrderStates.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <div class="state-item">
+        <div class="cmp-state-item">
             <el-checkbox-group>
                 <el-checkbox checked label="Option A">Pedido aceito</el-checkbox>
             </el-checkbox-group>
             <span>12:30</span>
         </div>
-        <div class="state-item">
+        <div class="cmp-state-item">
             <el-checkbox-group>
                 <el-checkbox checked label="Option B">Pedido em preparo</el-checkbox>
             </el-checkbox-group>
             <span>12:45</span>
         </div>
-        <div class="state-item">
+        <div class="cmp-state-item">
             <el-checkbox-group>
                 <el-checkbox label="Option C">Pronto para retirada</el-checkbox>
             </el-checkbox-group>
@@ -22,13 +22,13 @@
 </template>
 
 <style>
-.states-order{
+.cmp-states-order{
     text-align: center;
     width: 20em;
     border-bottom: 1px dashed #dcdcdc;
 }
 
-.state-item {
+.cmp-state-item {
     display: flex;
     gap: 8px;
     border-bottom: 2px #ccc dashed;
