043d3a185fecb5656dff6905cb89076dec80aee1
augusto <augustoabreu.202@gmail.com>
2023-10-02 20:31:18 -0400

    fix props card

--- src/views/SingleRestaurant.vue
+++ src/views/SingleRestaurant.vue
@@ -7,120 +7,119 @@ import Cardapio from '../components/Menu.vue'
 
 import { useRouter, useRoute } from 'vue-router'
 
-/*const pratos = [
- {
-   "nome": "Feijoada",
-   "valor": 25.99,
-   "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
-   "ingredientes": ["Feijão Preto", "Carne de Porco", "Arroz"],
-   "restaurante": 6,
-   "categoria_prato": 0, 
-   "desconto": false,
-   "valor_desconto": 0,
-   "descricao": "A feijoada é um prato brasileiro clássico, preparado com feijão preto e diversas carnes de porco, como linguiça e bacon. É um prato rico em sabor, acompanhado de arroz, couve e laranja."
- },
- {
-   "nome": "Sushi de Salmão",
-   "valor": 18.50,
-   "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
-   "ingredientes": ["Salmão Fresco", "Arroz de Sushi", "Alga Nori"],
-   "restaurante": 5, // Associando com o restaurante de índice 5
-   "categoria_prato": 1, // Associando com a categoria de índice 1 (Comida Japonesa)
-   "desconto": false,
-   "valor_desconto": 0,
-   "descricao": "Um prato japonês refinado composto por rolos de arroz de sushi, pedaços de salmão fresco e alga nori, oferecendo um sabor fresco e textura suave."
- },
- {
-   "nome": "Couscous Marroquino",
-   "valor": 12.99,
-   "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
-   "ingredientes": ["Couscous", "Vegetais", "Temperos"],
-   "restaurante": 4, // Associando com o restaurante de índice 4
-   "categoria_prato": 2, // Associando com a categoria de índice 2 (Comida Africana)
-   "desconto": false,
-   "valor_desconto": 0,
-   "descricao": "Uma iguaria da culinária marroquina, o couscous é preparado com sêmola de trigo, vegetais e uma mistura de temperos, resultando em um prato leve e aromático"
- },
- {
-   "nome": "Pizza Margherita",
-   "valor": 15.99,
-   "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
-   "ingredientes": ["Molho de Tomate", "Mozzarella", "Manjericão"],
-   "restaurante": 3, // Associando com o restaurante de índice 3
-   "categoria_prato": 0, // Associando com a categoria de índice 0 (Comida Brasileira)
-   "desconto": true,
-   "valor_desconto": 2.00,
-   "descricao": "A pizza clássica italiana apresenta uma base de massa coberta com molho de tomate, mozarela e folhas de manjericão, proporcionando uma explosão de sabores simples e deliciosos."
- },
- {
-   "nome": "Bobotie Sul-Africano",
-   "valor": 17.50,
-   "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
-   "ingredientes": ["Carne Moída", "Ovo", "Leite"],
-   "restaurante": 7, // Associando com o restaurante de índice 7
-   "categoria_prato": 2, // Associando com a categoria de índice 2 (Comida Africana)
-   "desconto": true,
-   "valor_desconto": 1.50,
-   "descricao": "Um prato autêntico da África do Sul, o bobotie é feito com carne moída temperada, ovos e leite, assado até dourar, oferecendo uma mistura única de sabores agridoces."
- },
- {
-   "nome": "Taco de Carne",
-   "valor": 8.99,
-   "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
-   "ingredientes": ["Carne de Bovino", "Tortilha", "Vegetais"],
-   "restaurante": 8, // Associando com o restaurante de índice 8
-   "categoria_prato": 2, // Associando com a categoria de índice 2 (Comida Africana)
-   "desconto": false,
-   "valor_desconto": 0,
-   "descricao": " Um prato da culinária mexicana, o taco de carne apresenta carne de bovino temperada, servida em uma tortilha macia e recheada com vegetais frescos, criando uma explosão de sabores."
- },
- {
-   "nome": "Tempurá de Camarão",
-   "valor": 21.99,
-   "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
-   "ingredientes": ["Camarão", "Vegetais", "Molho de Tempurá"],
-   "restaurante": 1, // Associando com o restaurante de índice 1
-   "categoria_prato": 1, // Associando com a categoria de índice 1 (Comida Japonesa)
-   "desconto": false,
-   "valor_desconto": 0,
-   "descricao": "Uma iguaria da culinária japonesa, o tempurá de camarão é preparado com camarões frescos, vegetais e molho de tempurá, proporcionando uma textura crocante e sabor requintado."
- },
- {
-   "nome": "Acarajé",
-   "valor": 7.50,
-   "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
-   "ingredientes": ["Feijão Fradinho", "Camarão Seco", "Dendê"],
-   "restaurante": 0, // Associando com o restaurante de índice 0
-   "categoria_prato": 0, // Associando com a categoria de índice 0 (Comida Brasileira)
-   "desconto": true,
-   "valor_desconto": 1.00,
-   "descricao": "Uma iguaria da culinária brasileira, o acarajé é uma bola frita feita de massa de feijão fradinho recheada com camarão seco e dendê. É um petisco tradicionalmente servido nas ruas da Bahia, com sabor único e uma textura crocante por fora e macia por dentro."
- },
- {
-   "nome": "Sobá Japonês",
-   "valor": 14.99,
-   "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
-   "ingredientes": ["Macarrão", "Caldo Dashi", "Vegetais"],
-   "restaurante": 2, // Associando com o restaurante de índice 2
-   "categoria_prato": 1, // Associando com a categoria de índice 1 (Comida Japonesa)
-   "desconto": true,
-   "valor_desconto": 1.50,
-   "descricao": "Um prato da culinária japonesa, o sobá é uma sopa de macarrão servida em um caldo à base de dashi, acompanhada de vegetais e outros ingredientes, proporcionando um sabor leve e refrescante."
- },
- {
-   "nome": "Baião de Dois",
-   "valor": 11.99,
-   "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
-   "ingredientes": ["Arroz", "Feijão Verde", "Queijo Coalho"],
-   "restaurante": 6, // Associando com o restaurante de índice 6
-   "categoria_prato": 0, // Associando com a categoria de índice 0 (Comida Brasileira)
-   "desconto": false,
-   "valor_desconto": 0,
-   "descricao": "Um prato típico da culinária nordestina do Brasil, o baião de dois é preparado com arroz e feijão verde cozidos juntos, misturados com queijo coalho e temperos. É uma refeição saborosa e reconfortante, com influências da cultura nordestina."
- }
-];
+  /*const pratos = [
+  {
+    "nome": "Feijoada",
+    "valor": 25.99,
+    "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
+    "ingredientes": ["Feijão Preto", "Carne de Porco", "Arroz"],
+    "restaurante": 6,
+    "categoria_prato": 0, 
+    "desconto": false,
+    "valor_desconto": 0,
+    "descricao": "A feijoada é um prato brasileiro clássico, preparado com feijão preto e diversas carnes de porco, como linguiça e bacon. É um prato rico em sabor, acompanhado de arroz, couve e laranja."
+  },
+  {
+    "nome": "Sushi de Salmão",
+    "valor": 18.50,
+    "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
+    "ingredientes": ["Salmão Fresco", "Arroz de Sushi", "Alga Nori"],
+    "restaurante": 5, // Associando com o restaurante de índice 5
+    "categoria_prato": 1, // Associando com a categoria de índice 1 (Comida Japonesa)
+    "desconto": false,
+    "valor_desconto": 0,
+    "descricao": "Um prato japonês refinado composto por rolos de arroz de sushi, pedaços de salmão fresco e alga nori, oferecendo um sabor fresco e textura suave."
+  },
+  {
+    "nome": "Couscous Marroquino",
+    "valor": 12.99,
+    "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
+    "ingredientes": ["Couscous", "Vegetais", "Temperos"],
+    "restaurante": 4, // Associando com o restaurante de índice 4
+    "categoria_prato": 2, // Associando com a categoria de índice 2 (Comida Africana)
+    "desconto": false,
+    "valor_desconto": 0,
+    "descricao": "Uma iguaria da culinária marroquina, o couscous é preparado com sêmola de trigo, vegetais e uma mistura de temperos, resultando em um prato leve e aromático"
+  },
+  {
+    "nome": "Pizza Margherita",
+    "valor": 15.99,
+    "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
+    "ingredientes": ["Molho de Tomate", "Mozzarella", "Manjericão"],
+    "restaurante": 3, // Associando com o restaurante de índice 3
+    "categoria_prato": 0, // Associando com a categoria de índice 0 (Comida Brasileira)
+    "desconto": true,
+    "valor_desconto": 2.00,
+    "descricao": "A pizza clássica italiana apresenta uma base de massa coberta com molho de tomate, mozarela e folhas de manjericão, proporcionando uma explosão de sabores simples e deliciosos."
+  },
+  {
+    "nome": "Bobotie Sul-Africano",
+    "valor": 17.50,
+    "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
+    "ingredientes": ["Carne Moída", "Ovo", "Leite"],
+    "restaurante": 7, // Associando com o restaurante de índice 7
+    "categoria_prato": 2, // Associando com a categoria de índice 2 (Comida Africana)
+    "desconto": true,
+    "valor_desconto": 1.50,
+    "descricao": "Um prato autêntico da África do Sul, o bobotie é feito com carne moída temperada, ovos e leite, assado até dourar, oferecendo uma mistura única de sabores agridoces."
+  },
+  {
+    "nome": "Taco de Carne",
+    "valor": 8.99,
+    "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
+    "ingredientes": ["Carne de Bovino", "Tortilha", "Vegetais"],
+    "restaurante": 8, // Associando com o restaurante de índice 8
+    "categoria_prato": 2, // Associando com a categoria de índice 2 (Comida Africana)
+    "desconto": false,
+    "valor_desconto": 0,
+    "descricao": " Um prato da culinária mexicana, o taco de carne apresenta carne de bovino temperada, servida em uma tortilha macia e recheada com vegetais frescos, criando uma explosão de sabores."
+  },
+  {
+    "nome": "Tempurá de Camarão",
+    "valor": 21.99,
+    "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
+    "ingredientes": ["Camarão", "Vegetais", "Molho de Tempurá"],
+    "restaurante": 1, // Associando com o restaurante de índice 1
+    "categoria_prato": 1, // Associando com a categoria de índice 1 (Comida Japonesa)
+    "desconto": false,
+    "valor_desconto": 0,
+    "descricao": "Uma iguaria da culinária japonesa, o tempurá de camarão é preparado com camarões frescos, vegetais e molho de tempurá, proporcionando uma textura crocante e sabor requintado."
+  },
+  {
+    "nome": "Acarajé",
+    "valor": 7.50,
+    "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
+    "ingredientes": ["Feijão Fradinho", "Camarão Seco", "Dendê"],
+    "restaurante": 0, // Associando com o restaurante de índice 0
+    "categoria_prato": 0, // Associando com a categoria de índice 0 (Comida Brasileira)
+    "desconto": true,
+    "valor_desconto": 1.00,
+    "descricao": "Uma iguaria da culinária brasileira, o acarajé é uma bola frita feita de massa de feijão fradinho recheada com camarão seco e dendê. É um petisco tradicionalmente servido nas ruas da Bahia, com sabor único e uma textura crocante por fora e macia por dentro."
+  },
+  {
+    "nome": "Sobá Japonês",
+    "valor": 14.99,
+    "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
+    "ingredientes": ["Macarrão", "Caldo Dashi", "Vegetais"],
+    "restaurante": 2, // Associando com o restaurante de índice 2
+    "categoria_prato": 1, // Associando com a categoria de índice 1 (Comida Japonesa)
+    "desconto": true,
+    "valor_desconto": 1.50,
+    "descricao": "Um prato da culinária japonesa, o sobá é uma sopa de macarrão servida em um caldo à base de dashi, acompanhada de vegetais e outros ingredientes, proporcionando um sabor leve e refrescante."
+  },
+  {
+    "nome": "Baião de Dois",
+    "valor": 11.99,
+    "imagem": "https://www.sabornamesa.com.br/media/k2/items/cache/7b98703f48b0025160f2b9b5aad2a874_XL.jpg",
+    "ingredientes": ["Arroz", "Feijão Verde", "Queijo Coalho"],
+    "restaurante": 6, // Associando com o restaurante de índice 6
+    "categoria_prato": 0, // Associando com a categoria de índice 0 (Comida Brasileira)
+    "desconto": false,
+    "valor_desconto": 0,
+    "descricao": "Um prato típico da culinária nordestina do Brasil, o baião de dois é preparado com arroz e feijão verde cozidos juntos, misturados com queijo coalho e temperos. É uma refeição saborosa e reconfortante, com influências da cultura nordestina."
+  }
+  ];
 */
-
 </script>
 <template>
     <Banner/>
