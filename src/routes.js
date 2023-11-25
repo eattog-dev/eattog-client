a66292a9e1e1baba789b3d1e16c1052a014d2d63
Milena dos Santos Carmona <97292640+MilenaCarmona@users.noreply.github.com>
2023-11-24 21:21:59 -0300

    complete page

--- src/routes.js
+++ src/routes.js
@@ -27,7 +27,7 @@ const routes = [
     { path: '/restaurants/:id', name: 'SingleRestaurant', props: true, query: true, component: SingleRestaurant },
     { path: '/admin', component: Admin },
     { path: '/dishes', name: Dishes, component: Dishes },
-    { path: '/gerenciar',name: ManageSystem , component: ManageSystem},
+    { path: '/adminconsole',name: ManageSystem , component: ManageSystem},
     { path: '/categorias/:id/:nome', name: SingleCategoria, component: SingleCategoria },
     { path: '/stateorder', name: StateOrder, component: StateOrder },
 
