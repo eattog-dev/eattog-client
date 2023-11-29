a75464bbd0ebdd8744444a6df3ad9646556d9453
Emanuel <emanuel.ibanhes.contato@gmail.com>
2023-11-28 21:41:40 -0400

    listagem de pratos

--- src/components/DishHome.vue
+++ src/components/DishHome.vue
@@ -1,99 +1,97 @@
 <script setup>
-    const props = defineProps({
-        dish: {
-            id: Number,
-            image: String,
-            nome: String,
-            ingredientes: String,
-            time: Number,
-            prato_categoria: String,
-            price: Number,
-            desconto: Boolean,
-            valor_desconto: Number
-        }
-    });
+const props = defineProps({
+  dish: {
+    id: Number,
+    image: String,
+    nome: String,
+    ingredientes: String,
+    time: Number,
+    prato_categoria: String,
+    price: Number,
+    desconto: Boolean,
+    valor_desconto: Number
+  }
+});
 
-    const emits = defineEmits('pratoClicado');
-    const handlePratoClicado = () => {
-        emits('pratoClicado', props.dish)
-    }
+const emits = defineEmits('pratoClicado');
+const handlePratoClicado = () => {
+  emits('pratoClicado', props.dish)
+}
 </script>
 
 <template>
-    <el-col :xs="24" :sm="12" :md="8" :lg="6" :span="8">
-        <el-card :body-style="{ padding: '0px' }">
-            <img :src="dish.imagem" class="image">
-            <!-- <span class="tag-aliment" v-if="dish.prato_categoria.categoria == null">celta</span> 
-                        <span class="tag-aliment" v-else>{{ dish.prato_categoria.categoria }}</span>  -->
-            <div style="padding: 14px; text-align: center;">
-                <span>{{ dish.nome }}</span>
-                <div class="dish-price">
-                    <span class="dish-price__discount">
-                        R$ {{ dish.valor }}
-                        <span v-if=dish.desconto class="dish-price__original">R$ {{ dish.valor_desconto }}</span>
-                    </span>
-                </div>
-                <div class="clearfix">
-                    <!-- <time class="time">{{ dish.time }} min</time> -->
-                    <el-button type="text" class="button" @click="handlePratoClicado()">Adicionar</el-button>
-                </div>
-            </div>
-        </el-card>
-    </el-col>
+  <el-col :xs="24" :sm="12" :md="8" :lg="6" :span="8">
+    <div class="cmp-card-config">
+      <div class="cmp-card-prato">
+        <img :src="dish.imagem" class="image">
+        <span class="tag-aliment" v-if="dish.prato_categoria.categoria == null">celta</span>
+        <span class="tag-aliment" v-else>{{ dish.prato_categoria.categoria }}</span>
+        <div style="padding: 14px; text-align: center;">
+          <span>{{ dish.nome }}</span>
+          <div class="dish-price">
+            <span class="dish-price__discount">
+              R$ {{ dish.valor }}
+              <span v-if=dish.desconto class="dish-price__original">R$ {{ dish.valor_desconto }}</span>
+            </span>
+          </div>
+          <div class="cmp-botao-adicionar">
+            <el-button type="text" class="button" @click="handlePratoClicado()">Adicionar</el-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </el-col>
 </template>
 
-<style>
-    #list-dishes a {
-        text-decoration: none;
-    }
+<style scoped>
+.cmp-card-prato {
+  height: 290px;
+  margin-bottom: 30px;
+  text-align: center;
+  background-color: var(--white100);
+  box-shadow: 0 0 12px rgba(0, 0, 0, 0.12);
+  border: 1px solid var(--white300);
+  color: var(--gray800);
+  text-decoration: none !important;
+  border-radius: 5px;
+  transition: 0.2s;
+  cursor: pointer;
+  position: relative;
+}
 
-    #list-dishes .el-card {
-        margin: 1rem 0;
-        height: 18.75rem;
-    }
+.cmp-card-config {
+  height: 100%;
+}
 
-    #list-dishes .time {
-        font-size: 0.813rem;
-        color: var(--gray300);
-    }
+.cmp-card-prato .tag-aliment {
+  position: absolute;
+  position: absolute;
+  right: 117px;
+  bottom: 92px;
+  z-index: 9999999;
+  background-color: var(--yellow100);
+  padding: 5px;
+  border-radius: 5px;
+  color: var(--white100);
+  font-weight: 600;
+}
 
-    #list-dishes .button {
-        padding: 0;
-        float: right;
-    }
+.cmp-card-prato .cmp-botao-adicionar .button {
+  color: var(--yellow100);
+}
 
-    #list-dishes .image {
-        width: 100%;
-        display: block;
-        object-fit: cover;
-        height: 12.5rem;
-    }
+.cmp-card-prato .cmp-botao-adicionar .button:hover {
+  color: var(--yellow100);
+}
 
-    #list-dishes .tag-aliment {
-        text-align: center;
-    }
+.cmp-card-prato:hover {
+  transform: scale(1.02);
 
-    #list-dishes .clearfix {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+}
 
-    #list-dishes .dish-price__discount {
-        color: var(--green100);
-        margin-bottom: .625rem;
-    }
-
-    #list-dishes .dish-price__original {
-        position: relative;
-        font-size: 0.75rem;
-        color: var(--gray200);
-        text-decoration: line-through;
-        margin-left: 0.375rem;
-    }
-
-    #list-dishes .dish-restaurant__divisor {
-        border-top: 0.125rem dashed var(--white200);
-        margin: 0.5rem 0;
-    }
+.cmp-card-prato .image {
+  width: 100%;
+  height: 180px;
+  border-radius: 5px 5px 0px 0px;
+}
 </style>
